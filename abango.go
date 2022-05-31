e8ddbcf881bfa7b50b0c5f29c0452e971d27a031
EricKIm <kimhi65@gmail.com>
2022-06-01 01:11:02 +0900

    220601-0111-Kim

--- abango.go
+++ abango.go
@@ -57,7 +57,7 @@ func RunServicePoint(RestHandler func(ask *AbangoAsk)) {
 
 	var wg sync.WaitGroup
 
-	e.AokLog("Abango Clustered Framework Started !uuuuuu")
+	e.AokLog("Abango Clustered Framework Started !pppppp")
 	if err := GetXConfig(); err == nil {
 		if XConfig["XDBOn"] == "Yes" {
 			MyLinkXDB()
