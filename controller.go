34d038f5f7568787b00b98e9dc6fe8c3f5d006fb
EricKIm <kimhi65@gmail.com>
2022-05-31 11:09:38 +0900

    220531-1109-Kim

--- controller.go
+++ controller.go
@@ -2,6 +2,7 @@ package abango
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 	"time"
 
@@ -75,9 +76,9 @@ func (c *Controller) GetYDB() (int, string) {
 		}
 	}
 
-	// fmt.Println("YDB-kkkk")
-	// fmt.Println(c.Gtb.ConnString)
-	// fmt.Println("YDB-qqqq")
+	fmt.Println("YDB-kkkk")
+	fmt.Println(c.Gtb.ConnString)
+	fmt.Println("YDB-qqqq")
 	if c.Db, err = xorm.NewEngine(XConfig["DbType"], c.Gtb.ConnString); err != nil {
 		return 609, e.LogStr("ADASEF", "DBEngine Open Error")
 	}
