88b7df5cb1895aa7bc3bf81616df33f84cfa2a44
EricKIm <kimhi65@gmail.com>
2023-06-25 21:12:04 +0900

    230625-2112-Kim

--- abango.go
+++ abango.go
@@ -330,11 +330,11 @@ func MyLinkCrystalDB() { // Crystal Report Server
 	CrystalDB.SetMaxOpenConns(100)
 	CrystalDB.SetMaxIdleConns(20)
 	CrystalDB.SetConnMaxLifetime(60 * time.Second)
-	if _, err := CrystalDB.IsTableExist("aaa"); err != nil { //Connect Check
-		e.OkLog("JHGKIUGBJ-CrystalDB Unconnected " + strArr[1])
-		// e.MyErr("JHGKIUGBJ-CrystalDB Unconnected ", err, true)
-	} else {
-		e.OkLog("CrystalDB Connected :" + strArr[1])
-	}
+	// if _, err := CrystalDB.IsTableExist("aaa"); err != nil { //Connect Check
+	// 	e.OkLog("JHGKIUGBJ-CrystalDB Unconnected " + strArr[1])
+	// 	// e.MyErr("JHGKIUGBJ-CrystalDB Unconnected ", err, true)
+	// } else {
+	// 	e.OkLog("CrystalDB Connected :" + strArr[1])
+	// }
 
 }
