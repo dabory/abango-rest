d728d87832cd95f62853f9ab5765ad08a1a088d3
EricKIm <kimhi65@gmail.com>
2023-07-07 19:59:02 +0900

    230707-1959-Kim

--- kafka.go
+++ kafka.go
@@ -20,6 +20,7 @@ func init() {
 	KAFKA_TIMEOUT = XConfig["KafkaTimeout"]
 }
 
+// sdjlsdajadsl
 func KafkaProducer(key string, message string, conCurr string) (int32, int64, error) {
 
 	kfcf := sarama.NewConfig()
