2e5ff4e4e12e0146e79c9829a3ef455cc9efbf4e
EricKIm <kimhi65@gmail.com>
2023-10-01 18:14:46 +0900

    231001-1814-Kim

---
+++ search/spider-func.go
@@ -0,0 +1,38 @@
+// Author : Eric Kim
+// Build Date : 23 Jul 2018  Last Update 02 Aug 2018
+// End-Agent for Passcon Multi OS go binding with Windows, MacOS, iOS, and Android
+// All rights are reserved.
+
+package search
+
+import (
+	"fmt"
+	"reflect"
+
+	e "github.com/dabory/abango-rest/etc"
+)
+
+func StructToMap(in interface{}, tag string) (map[string]interface{}, error) {
+	out := make(map[string]interface{})
+
+	v := reflect.ValueOf(in)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	// we only accept structs
+	if v.Kind() != reflect.Struct {
+		fmt.Errorf("ToMap only accepts structs; got %T", v)
+		return nil, e.MyErr("HJKMNHGYUH-only accepts structs:", nil, false)
+	}
+
+	typ := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		// gets us a StructField
+		fi := typ.Field(i)
+		if tagv := fi.Tag.Get(tag); tagv != "" {
+			out[tagv] = v.Field(i).Interface()
+		}
+	}
+	return out, nil
+}
