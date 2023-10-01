60465cb671f0262ec02c4f14f98f19cc4ad84e12
EricKIm <kimhi65@gmail.com>
2023-10-01 18:06:25 +0900

    231001-1806-Kim

---
+++ search/struct.go
@@ -0,0 +1,41 @@
+type ItemInfo struct {
+	PdplinkHash string
+	TopUrl      string
+	TargetPath  string // URL without scheme info
+	HtmlHash    string
+	HubUrls     string
+	Categories  string
+	Emails      string
+	ItemName    string
+	Tags        string
+	Currency    string
+	SalesPrice  string
+
+	Images    string
+	ShortDesc string
+	TextDesc  string
+
+	OgMetas string
+	Options []struct {
+		Name    string
+		Choices []struct {
+			Name  string
+			Price string
+		}
+	}
+
+	OriginDesc    string
+	Manufacturer  string
+	Origin        string
+	Language      string
+	DeliveryPrice string
+	Sku           string
+	// ItemNick      string
+	// ModelName     string
+	// ModelNo       string
+	// BrandName     string
+	// MinimumQty    float32
+	// UserCredit    float32
+	// Suggest     []string
+	// Cats        []string
+}
